a37fbcde63229480550ee3b215e39c5c2300e8db
SONUKUMARSAW <sonukumarsaw66@gmail.com>
2020-07-23 01:25:58 +0530

    cmd: added function to retrive directory to be monitored

---
+++ cmd/cmd.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+//Flags type contains the command line flag values
+type Flags struct {
+	Interval  int    //time interval between each scans (in seconds)
+	RulesFile string //file name to load rules
+}
+
+//ParseFlag parse command line argument and return pointer to the struct type.
+func ParseFlag() *Flags {
+
+	interval := flag.Int("t", 5, "Time between scans (default is 5 seconds)")
+	rulesfile := flag.String("f", "rules", "Load rules from this file (default is 'rules')")
+
+	flag.Parse()
+
+	return &Flags{
+		Interval:  *interval,
+		RulesFile: *rulesfile,
+	}
+}
+
+//FetchDirectory fetch directory name to be monitored from the command line argument
+//It returns Fullpath and Basepath to that directory
+func FetchDirectory() (string, string) {
+
+	args := os.Args
+	size := len(args)
+	if size == 1 {
+		log.Fatalln("Please mention directory/file to be monitored")
+	}
+	//last element from of the arguments slice
+	basepath := args[size-1]
+	//current directory
+	fullpath, _ := os.Getwd()
+
+	if basepath != "." {
+		fullpath += basepath
+	}
+	//check if directory/file exists or not
+	if _, err := os.Stat(fullpath); os.IsNotExist(err) {
+		log.Fatalln("Directory Doesn't exists")
+	}
+
+	return basepath, fullpath
+}
